ee605ce1049ea1ff9a579f4409eb4657bf598f74
ImperatorNeron <katebarspk@gmail.com>
2023-03-25 00:38:13 +0200

    Added registration and login to index page

--- event_planner/events/static/events/js/registration.js
+++ event_planner/events/static/events/js/registration.js
@@ -1,10 +1,20 @@
 const loginButton = document.querySelector('.login');
+const regButton = document.querySelector('.register');
 
-loginButton.addEventListener('click', function () {
-    const regForm = document.querySelector('.reg-form');
+regButton.addEventListener('click', function () {
+    const block = document.querySelector('.block');
     const container = document.querySelector('.container');
     const reg_block = document.querySelector('.reg-block');
-    regForm.style.display = 'block';
+    block.style.display = 'block';
     container.style.filter = 'blur(5px)';
     reg_block.style.display = 'block';
+});
+
+loginButton.addEventListener('click', function () {
+    const block = document.querySelector('.block');
+    const container = document.querySelector('.container');
+    const log_block = document.querySelector('.login-block');
+    block.style.display = 'block';
+    container.style.filter = 'blur(5px)';
+    log_block.style.display = 'block';
 });
--- event_planner/events/views.py
+++ event_planner/events/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .forms import RegisterForm
 
 
-class Index(TemplateView):
-    template_name = "events/index.html"
+class RegisterLoginView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        login_form = AuthenticationForm()
+        return render(request, 'events/index.html', {'register_form': register_form, 'login_form': login_form})
 
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        login_form = AuthenticationForm(request, data=request.POST)
+
+        if register_form.is_valid():
+            user = register_form.save()
+            email = register_form.cleaned_data.get('email')
+            raw_password = register_form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=raw_password)
+            login(request, user)
+            return redirect('index')
+
+        elif login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+        return render(request, 'events/index.html', {'register_form': register_form, 'login_form': login_form})
