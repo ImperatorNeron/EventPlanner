7f677eef2db52ceefd57779c895040bb05e9fb92
ImperatorNeron <katebarspk@gmail.com>
2023-04-01 20:48:34 +0300

    stylized registration and login forms

--- event_planner/events/forms.py
+++ event_planner/events/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, forms
 from django.contrib.auth.models import User
 
 
@@ -6,3 +6,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+        widgets = {
+            "username": forms.TextInput(attrs={'class': 'username'}),
+            "email": forms.EmailInput(attrs={'class': 'email'}),
+            "password1": forms.PasswordInput(attrs={'class': 'password'}),
+            "password2": forms.PasswordInput(attrs={'class': 'password'}),
+        }
--- event_planner/events/static/events/js/registration.js
+++ event_planner/events/static/events/js/registration.js
@@ -1,5 +1,7 @@
 const loginButton = document.querySelector('.login');
 const regButton = document.querySelector('.register');
+const closeButton = document.querySelectorAll('.close-btn');
+const submitButton = document.querySelector('.form-btn');
 
 regButton.addEventListener('click', function () {
     const block = document.querySelector('.block');
@@ -18,3 +20,18 @@ loginButton.addEventListener('click', function () {
     container.style.filter = 'blur(5px)';
     log_block.style.display = 'block';
 });
+
+function close() {
+    const block = document.querySelector('.block');
+    const container = document.querySelector('.container');
+    const reg_block = document.querySelector('.reg-block');
+    const log_block = document.querySelector('.login-block');
+    block.style.display = 'none';
+    container.style.filter = 'none';
+    reg_block.style.display = 'none';
+    log_block.style.display = 'none';
+}
+
+
+closeButton[0].addEventListener('click', close);
+closeButton[1].addEventListener('click', close);
