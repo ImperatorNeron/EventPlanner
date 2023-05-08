aa9272b0735d93ddee1fc91d65af90e876839f3f
ImperatorNeron <katebarspk@gmail.com>
2023-05-08 15:25:54 +0300

    rewrite settings

--- event_planner/event_planner/settings.py
+++ event_planner/event_planner/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.56.1", "192.168.0.107", "127.0.0.1"]
 
 # Application definition
 
